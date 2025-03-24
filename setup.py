edeea7088c8047e4fb0eb1573e7591dd69b04cf3
joaomonteiro <joaopecomonteiro@gmail.com>
2025-03-24 16:54:56 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'PyTomo',
-    version = '0.1.0',
-    packages = find_packages(),
+    version = '0.0.1',
+    license = 'MIT',
+    author = 'SpaceODT',
+    packages = ['PyTomo'],
+    url='https://github.com/cmcorreia/PyTomo',
     install_requires = [
     'aotools',
     'numpy<2.0.0'
